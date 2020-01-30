31079f9e9c9de1facde38a51ca393279c650c59f
Anthony Giuliano <agiuliano@gmail.com>
2020-01-30 07:09:55 -1000

    adds unit tests

--- dominion/unittest_Dominion.py
+++ dominion/unittest_Dominion.py
@@ -120,6 +120,103 @@ class TestCard(TestCase):
         self.assertEqual(buys, self.player.buys)
         self.assertEqual(coins, self.player.purse)
 
+    def test_player_action_balance(self):
+        self.setup()
+        multiple = 70
+        cards = len(self.player.stack())
+        balance = 0
+
+        for c in self.player.stack():
+            if c.category == 'action':
+                balance -= 1
+                balance += c.actions
+
+        self.assertEqual(multiple * balance / len(self.player.stack()), self.player.action_balance())
+
+    def test_player_action_balance_with_action(self):
+        self.setup()
+        multiple = 70
+        name = 'Test Name'
+        cost = 1
+        actions = 2
+        cards = 3
+        buys = 4
+        coins = 5
+        card = Dominion.Action_card(name, cost, actions, cards, buys, coins)
+        self.player.hand.append(card)
+        cards = len(self.player.stack())
+        balance = 0
+
+        for c in self.player.stack():
+            if c.category == 'action':
+                balance -= 1
+                balance += c.actions
+
+        self.assertEqual(multiple * balance / len(self.player.stack()), self.player.action_balance())
+
+
+    def test_player_calcpoints(self):
+        self.setup()
+        points = 0
+        self.player.hand.append(Dominion.Province())
+        points += Dominion.Province().vpoints
+        self.player.hand.append(Dominion.Duchy())
+        points += Dominion.Duchy().vpoints
+        self.player.hand.append(Dominion.Estate())
+        points += Dominion.Estate().vpoints
+        self.player.hand.append(Dominion.Gardens())
+        num_gardens = 4
+        garden_vpoint_const = 10
+        points += (len(self.player.stack()) // garden_vpoint_const) * num_gardens
+
+        self.assertEqual(self.player.calcpoints(), points)
+
+
+    def test_player_draw(self):
+        self.setup()
+        name = 'Test Name'
+        cost = 1
+        actions = 2
+        cards = 3
+        buys = 4
+        coins = 5
+        card = Dominion.Action_card(name, cost, actions, cards, buys, coins)
+        self.player.discard.append(card)
+        self.player.deck = []
+        self.player.draw()
+
+        self.assertEqual(self.player.discard, [])
+        self.assertEqual(self.player.deck, [])
+        self.assertEqual(self.player.hand[-1], card)
+
+    def test_player_card_summary(self):
+        self.setup()
+        self.player.hand.append(Dominion.Province())
+        self.player.hand.append(Dominion.Duchy())
+        self.player.hand.append(Dominion.Estate())
+        self.player.hand.append(Dominion.Gardens())
+        summary = self.player.cardsummary()
+        num_estates = 4
+
+        self.assertEqual(self.player.calcpoints(), summary['VICTORY POINTS'])
+        self.assertEqual(num_estates, summary[Dominion.Estate().name])
+
+
+    def test_game_over_province(self):
+        self.setup()
+        self.supply['Province'] = []
+        self.assertEqual(Dominion.gameover(self.supply), True)
+
+    def test_game_over_supply(self):
+        self.setup()
+        self.supply['Copper'] = []
+        self.supply['Silver'] = []
+        self.supply['Gold'] = []
+        self.assertEqual(Dominion.gameover(self.supply), True)
+
+    def test_game_not_over(self):
+        self.setup()
+        self.assertNotEqual(Dominion.gameover(self.supply), True)
 
     def test_react(self):
         pass
